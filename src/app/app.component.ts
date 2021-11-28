212dbd34f89b51f15518ac15f6b236ea57c6b10e
mheshamm <mheshamm96@gmail.com>
2021-11-28 16:35:12 +0200

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('movingText', [
+      state('on', style({left: '-300px'})),
+      transition('* => *', [
+        style({left: '-300px'}),
+        animate(15000, style({left: '100%'}))
+      ])
+    ])
+  ]
 })
+
+
 export class AppComponent {
   title = 'task';
+  state = 0;
+
+  moveEnd() {
+    this.state++;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { SvgLineComponent } from './components/svg-line/svg-line.component';
 
 import { ChartComponent } from './components/chart/chart.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -17,8 +18,9 @@ import { ChartComponent } from './components/chart/chart.component';
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
-    CommonModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
